f4cdfd3ad9c2661c8b466e98bc19d35552bf4370
Dyllan García <dyllangm193@gmail.com>
2024-06-07 19:43:57 -0600

    Parent & Child

--- Practica 1/Child.c
+++ Practica 1/Child.c
@@ -1,18 +1,64 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
-#include <sys/types.h>
+#include <fcntl.h>
+#include <time.h>
 #include <string.h>
-#include <sys/wait.h>
 
-int main(int argc, char *argv) {
-	// printf("Proceso hijo\n");
-	// printf("PID: %d\n", getpid());
+#define FILENAME "practica1.txt"
 
-	printf("Argumento 1: %s\n", argv[1]);
-	printf("Argumento 2: %s\n", argv[2]);
+void random_string(char *str, size_t size) {
+    const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    if (size) {
+        --size;
+        for (size_t n = 0; n < size; n++) {
+            int key = rand() % (int) (sizeof charset - 1);
+            str[n] = charset[key];
+        }
+        str[size] = '\0';
+    }
+}
 
-	sleep(4);
+void perform_syscalls() {
+    int fd = open(FILENAME, O_RDWR | O_CREAT | O_TRUNC, 0644);
+    if (fd == -1) {
+        perror("open");
+        exit(1);
+    }
 
-	exit(0);
+    char buffer[9];
+    buffer[8] = '\0';
+    while (1) {
+        int syscall_type = rand() % 3;
+        switch (syscall_type) {
+            case 0: // Write
+                random_string(buffer, 8);
+                write(fd, buffer, 8);
+                break;
+            case 1: // Read
+                lseek(fd, 0, SEEK_SET);
+                read(fd, buffer, 8);
+                break;
+            case 2: // Open (reopen the file)
+                close(fd);
+                fd = open(FILENAME, O_RDWR);
+                if (fd == -1) {
+                    perror("open");
+                    exit(1);
+                }
+                break;
+        }
+        sleep(1 + rand() % 3) // random exec time 
+    }
+}
+
+int main(int argc, char *argv[]) {
+    printf("Argumento 1: %s\n", argv[1]);
+    printf("Argumento 2: %s\n", argv[2]);
+
+    srand(time(NULL));
+    perform_syscalls();
+
+    // Exit code
+    exit(1);
 }
--- Practica 1/Parent.c
+++ Practica 1/Parent.c
@@ -4,39 +4,101 @@
 #include <sys/types.h>
 #include <string.h>
 #include <sys/wait.h>
+#include <signal.h>
+#include <time.h>
+#include <sys/ptrace.h>
+#include <sys/user.h>
+#include <sys/syscall.h>
+#include <fcntl.h>
+
+#define NUM_CHILDREN 2
+
+int syscall_count = 0;
+int syscall_type_count[1024] = {0}; // Asumimos un máximo de 1024 tipos de llamadas al sistema
+FILE *log_file;
+
+void handle_sigint(int sig) {
+    printf("Número total de llamadas al sistema: %d\n", syscall_count);
+    for (int i = 0; i < 1024; i++) {
+        if (syscall_type_count[i] > 0) {
+            printf("Syscall %d: %d\n", i, syscall_type_count[i]);
+        }
+    }
+    fclose(log_file);
+    exit(0);
+}
+
+void log_syscall(pid_t pid, int syscall) {
+    time_t now = time(NULL);
+    struct tm *t = localtime(&now);
+
+    fprintf(log_file, "Proceso %d: syscall %d (%04d-%02d-%02d %02d:%02d:%02d)\n", pid, syscall,
+            t->tm_year + 1900, t->tm_mon + 1, t->tm_mday, t->tm_hour, t->tm_min, t->tm_sec);
+    fflush(log_file);
+    syscall_count++;
+    syscall_type_count[syscall]++;
+}
+
+void trace_child(pid_t child_pid) {
+    int status;
+    printf("Esperando a proceso hijo con PID: %d\n", child_pid);
+    ptrace(PTRACE_SYSCALL, child_pid, 0, 0); // Empezar la traza
+    while (1) {
+        waitpid(child_pid, &status, 0);
+        if (WIFEXITED(status)) {
+            printf("Proceso hijo con PID %d terminó\n", child_pid);
+            break;
+        }
+        if (WIFSTOPPED(status)) {
+            struct user_regs_struct regs;
+            if (ptrace(PTRACE_GETREGS, child_pid, 0, &regs) == -1) {
+                perror("ptrace GETREGS");
+                exit(1);
+            }
+            printf("Proceso %d realizó syscall %lld\n", child_pid, regs.orig_rax);
+            log_syscall(child_pid, regs.orig_rax);
+        }
+        if (ptrace(PTRACE_SYSCALL, child_pid, 0, 0) == -1) {
+            perror("ptrace SYSCALL");
+            exit(1);
+        }
+    }
+}
 
 int main() {
-	printf("INICIO");
+    pid_t children[NUM_CHILDREN];
+    signal(SIGINT, handle_sigint);
+    log_file = fopen("syscalls.log", "w");
 
-	pid_t pid = fork();
+    if (log_file == NULL) {
+        perror("No se pudo abrir el archivo syscalls.log");
+        exit(1);
+    }
 
-	if(pid == -1) {
-		perror("fork");
-		exit(1);
-	}
+    printf("INICIO\n");
 
-	if(pid) {
-		printf("Proceso padre: %d\n", pid);
-		printf("PID: %d\n\n", getpid());
+    for (int i = 0; i < NUM_CHILDREN; i++) {
+        if ((children[i] = fork()) == 0) {
+            ptrace(PTRACE_TRACEME, 0, 0, 0);
+            kill(getpid(), SIGSTOP); // Esperar hasta que el padre esté listo para rastrear
+            execl("./child", "./child", "Hola", "Soy el Proceso Hijo!", NULL);
+            perror("execl");
+            exit(1);
+        } else if (children[i] == -1) {
+            perror("fork");
+            exit(1);
+        } else {
+            printf("Proceso hijo creado con PID: %d\n", children[i]);
+        }
+    }
 
-		int status;
-		wait(&status);
+    for (int i = 0; i < NUM_CHILDREN; i++) {
+        // Esperar a que el hijo se detenga
+        waitpid(children[i], NULL, 0);
+        ptrace(PTRACE_SYSCALL, children[i], 0, 0); // Empezar la traza
+        trace_child(children[i]);
+    }
 
-		if(WIFEXITED(status)) {
-			printf("\nEl proceso hijo termino con estado: %d\n", status);
-		} else {
-			printf("\nError el proceso hijo termino con estado %d\n", status);
-		}
-
-		printf("Terminando el proceso padre\n");
-	} else {
-		// printf("Proceso hijo: %d\n", pid);
-		// printf("PID: %d\n", getpid());
-		char *arg_Ptr[4];
-		arg_Ptr[0] = " child.bin";
-		arg_Ptr[1] = " Hola";
-		arg_Ptr[2] = " Soy el Proceso Hijo! ";
-		arg_Ptr[3] = NULL;
-		execv("./child.bin", arg_Ptr);
-	}
+    fclose(log_file);
+    return 0;
 }
