278d3f7fa371909ce72833cae21f901560313b8b
robinbuezo11 <3384471212007@ingenieria.usac.edu.gt>
2024-06-26 00:20:11 -0600

    [update] proyecto dml

--- Proyecto/Script/DML.sql
+++ Proyecto/Script/DML.sql
@@ -1,5 +1,14 @@
 SELECT * FROM Memoria;
 
-SELECT pid, nombre, GREATEST(SUM(CASE WHEN tipo = 'mmap2' THEN tamano ELSE tamano*-1 END),0) memoria
+SELECT  pid,
+        nombre,
+        GREATEST(SUM(CASE WHEN tipo = 'mmap2' THEN tamano ELSE tamano * -1 END),0)/1024/1024 memoria_mb,
+        ROUND(GREATEST(SUM(CASE WHEN tipo = 'mmap2' THEN tamano ELSE tamano * -1 END),0) /
+            (SELECT SUM(porcentaje)
+            FROM (SELECT GREATEST(SUM(CASE WHEN tipo = 'mmap2' THEN tamano ELSE tamano * -1 END), 0) porcentaje
+                  FROM Memoria
+                  GROUP BY pid, nombre) total
+            ) * 100, 2) porcentaje
 FROM Memoria
 GROUP BY pid, nombre
+ORDER BY pid
