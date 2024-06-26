f81739efd4e4d46ffa3802e3ec12aa96b9398df5
dyllanRodrigo <70991982+dyllanRodrigo@users.noreply.github.com>
2024-06-25 23:44:44 -0600

    Init Reset

--- Proyecto/Frontend/memory-dashboard/src/App.js
+++ Proyecto/Frontend/memory-dashboard/src/App.js
@@ -1,43 +1,25 @@
-import React, { useEffect, useState } from 'react';
-import io from 'socket.io-client';
-import MemoryPieChart from './components/MemoryPieChart';
-import ProcessTable from './components/ProcessTable';
-import RequestTable from './components/RequestTable';
+import logo from './logo.svg';
 import './App.css';
 
-const socket = io('http://localhost:3001'); // Cambia la URL según tu API
-
-const App = () => {
-  const [memoryData, setMemoryData] = useState([]);
-  const [processes, setProcesses] = useState([]);
-  const [requests, setRequests] = useState([]);
-
-  useEffect(() => {
-    socket.on('memoryData', (data) => {
-      setMemoryData(data.pieChartData);
-      setProcesses(data.processes);
-      setRequests(data.requests);
-    });
-
-    return () => {
-      socket.off('memoryData');
-    };
-  }, []);
-
+function App() {
   return (
     <div className="App">
-      <h1>Dashboard</h1>
-      <div className="charts">
-        <MemoryPieChart data={memoryData} />
-      </div>
-      <div className="tables">
-        <h2>Procesos</h2>
-        <ProcessTable processes={processes} />
-        <h2>Solicitudes</h2>
-        <RequestTable requests={requests} />
-      </div>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
-};
+}
 
 export default App;
