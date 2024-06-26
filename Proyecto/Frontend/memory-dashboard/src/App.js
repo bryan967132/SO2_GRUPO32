d9f56cea2b12df4d301346af1dd84822ff07d2ed
dyllanRodrigo <70991982+dyllanRodrigo@users.noreply.github.com>
2024-06-26 00:32:36 -0600

    ProjectData

--- Proyecto/Frontend/memory-dashboard/src/App.js
+++ Proyecto/Frontend/memory-dashboard/src/App.js
@@ -54,6 +54,9 @@ const App = () => {
 
   return (
     <div className="App">
+      <h1>Proyecto - Lab SOPES 2</h1>
+      <h3>201907774, 201908355 , 201944994</h3>
+
       <h1>Dashboard</h1>
       {error && <p className="error">{error}</p>}
       <div className="charts">
