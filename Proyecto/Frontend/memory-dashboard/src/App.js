f98551cebbc9c59551b15ee0f0d94fd856303c08
dyllanRodrigo <70991982+dyllanRodrigo@users.noreply.github.com>
2024-06-26 23:23:42 -0600

    SetInterval Refresh

--- Proyecto/Frontend/memory-dashboard/src/App.js
+++ Proyecto/Frontend/memory-dashboard/src/App.js
@@ -13,12 +13,10 @@ const App = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
-        // Fetch data from procesos endpoint
         const responseProcesos = await fetch('http://18.191.237.75:5000/api/procesos');
         const dataProcesos = await responseProcesos.json();
         setProcesses(dataProcesos);
 
-        // Transform data for pie chart
         const transformedPieChartData = dataProcesos.map(process => ({
           name: process.nombre,
           memory: parseFloat(process.memoria_mb),
@@ -26,24 +24,24 @@ const App = () => {
         }));
         setMemoryData(transformedPieChartData);
 
-        // Fetch data from solicitudes endpoint
         const responseSolicitudes = await fetch('http://18.191.237.75:5000/api/solicitudes');
         const dataSolicitudes = await responseSolicitudes.json();
         setRequests(dataSolicitudes);
-
       } catch (error) {
         setError('No se pudo obtener los datos del servidor');
       }
     };
 
     fetchData();
+
+    const interval = setInterval(fetchData, 2000); // Actualiza cada 2 segundos
+
+    return () => clearInterval(interval); // Limpia el intervalo al desmontar el componente
   }, []);
 
   return (
     <div className="App">
       <h1>Dashboard</h1>
-      <h1>Proyecto - Lab SOPES 2</h1>
-      <h3>201907774, 201908355 , 201944994</h3>
       {error && <p className="error">{error}</p>}
       <div className="charts">
         <MemoryPieChart data={memoryData} />
